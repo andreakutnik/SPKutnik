df9e49556e2186e3729b877268368b232bceba6d
andreakutnik <ANDREA.KUTNIK00@E-UVT.RO>
2023-11-15 18:36:57 +0200

    commit lab04

--- src/main/java/com/example/spkutnik/SpKutnikApplication.java
+++ src/main/java/com/example/spkutnik/SpKutnikApplication.java
@@ -3,29 +3,36 @@ package com.example.spkutnik;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class SpKutnikApplication {
 
-    public static void main(String[] args) {
-        Book noapteBuna = new Book("Noapte buna, copii!");
-        Author rpGheo = new Author("Radu Pavel Gheo");
-        noapteBuna.addAuthor(rpGheo);
-        Section cap1 = new Section("Capitolul 1");
-        Section cap11 = new Section("Capitolul 1.1");
-        Section cap111 = new Section("Capitolul 1.1.1");
-        Section cap1111 = new Section("Subchapter 1.1.1.1");
-        noapteBuna.addContent(new Paragraph("Multumesc celor care ..."));
-        noapteBuna.addContent(cap1);
-        cap1.add(new Paragraph("Moto capitol"));
-        cap1.add(cap11);
-        cap11.add(new Paragraph("Text from subchapter 1.1"));
-
-        cap11.add(cap111);
-        cap111.add(new Paragraph("Text from subchapter 1.1.1"));
-        cap111.add(cap1111);
-        cap1111.add(new Image("Image subchapter 1.1.1.1"));
-
-        noapteBuna.print();
+    public static void main(String[] args) throws Exception {
+        long startTime = System.currentTimeMillis();
+        ImageProxy img1 = new ImageProxy("Pamela Anderson");
+        ImageProxy img2 = new ImageProxy("Kim Kardashian");
+        ImageProxy img3 = new ImageProxy("Kirby Griffin");
+        Section playboyS1 = new Section("Front Cover");
+        playboyS1.add(img1);
+        Section playboyS2 = new Section("Summer Girls");
+        playboyS2.add(img2);
+        playboyS2.add(img3);
+        Book playboy = new Book("Playboy");
+        playboy.addContent(playboyS1);
+        playboy.addContent(playboyS2);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Creation of the content took " + (endTime -
+                startTime) + " milliseconds");
+        startTime = System.currentTimeMillis();
+        playboyS1.print();
+        endTime = System.currentTimeMillis();
+        System.out.println("Printing of the section 1 took " + (endTime -
+                startTime) + " milliseconds");
+        startTime = System.currentTimeMillis();
+        playboyS1.print();
+        endTime = System.currentTimeMillis();
+        System.out.println("Printing again the section 1 took " + (endTime -
+                startTime) + " milliseconds");
     }
 
     static class Book {
@@ -110,14 +117,34 @@ public class SpKutnikApplication {
             }
         }
     }
+    public static class ImageProxy extends Content {
+        private Image realImage;
+        public String name;
 
-    static class Image extends Content {
-        private String name;
-
-        public Image(String name) {
+        public ImageProxy(String name) {
             this.name = name;
         }
 
+        @Override
+        public void print() {
+            if (realImage == null) {
+                realImage = new Image(name);
+            }
+            realImage.print();
+        }
+    }
+
+    static class Image extends ImageProxy {
+        public Image(String name) {
+            super(name);
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
         public void print() {
             System.out.println("Image with name: " + this.name);
         }
