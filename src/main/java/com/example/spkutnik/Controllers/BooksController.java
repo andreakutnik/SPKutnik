3354431268492f07dd195626dd83c9f940e42597
andreakutnik <ANDREA.KUTNIK00@E-UVT.RO>
2024-01-18 00:55:37 +0200

    lab 09-10 Chain of Responsibility and Command

---
+++ src/main/java/com/example/spkutnik/Controllers/BooksController.java
@@ -0,0 +1,76 @@
+package com.example.spkutnik.Controllers;
+
+import com.example.spkutnik.models.Book;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BooksController {
+
+    private List<Book> books = new ArrayList<>();
+
+    // GET /books
+    @GetMapping
+    public ResponseEntity<List<Book>> getAllBooks() {
+        return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+
+    // GET /books/{id}
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable long id) {
+        Book foundBook = findBookById(id);
+        if (foundBook != null) {
+            return new ResponseEntity<>(foundBook, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // POST /books
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody Book newBook) {
+        books.add(newBook);
+        return new ResponseEntity<>(newBook, HttpStatus.CREATED);
+    }
+
+    // PUT /books/{id}
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable long id, @RequestBody Book updatedBook) {
+        Book existingBook = findBookById(id);
+        if (existingBook != null) {
+            existingBook.setId(updatedBook.getId());
+            existingBook.setTitle(updatedBook.getTitle());
+            existingBook.setAuthors(updatedBook.getAuthors());
+            existingBook.setElements(updatedBook.getElements());
+
+            return new ResponseEntity<>(existingBook, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // DELETE /books/{id}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable long id) {
+        Book existingBook = findBookById(id);
+        if (existingBook != null) {
+            books.remove(existingBook);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    private Book findBookById(long id) {
+        return books.stream()
+                .filter(book -> book.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+}
+
