60e284b0bf116a92dbb5d3058edfd42202c1e5df
andreakutnik <ANDREA.KUTNIK00@E-UVT.RO>
2024-01-17 20:00:25 +0200

    lab 05 dependency injection

---
+++ src/main/java/com/difexample/SingletonComponent.java
@@ -0,0 +1,16 @@
+package com.difexample;
+
+import org.springframework.stereotype.Component;
+@Component
+public class SingletonComponent {
+    public SingletonComponent() {
+        System.out.println(
+                "SingletonComponent::SingletonComponent = " + this);
+
+    }
+    public void operation() {
+        System.out.println(
+                "Invoked SingletonComponent::operation() on " + this);
+
+    }
+}
